0a662e87873c6abd75d3d0703b30b22700558d86
roshila <79713928+roshila@users.noreply.github.com>
2022-08-16 17:17:28 +0300

    Складываем строки без регистрации и смс

--- py/hello.py
+++ py/hello.py
@@ -1,1 +1,7 @@
-print("Hello," + input("What is your name?"))
+result1 = 50 + 125
+result2 = str(result1)
+print("Result" + result2)
+
+result = 123 + 41
+result = str(result)
+print("Так тоже можно" + result)
