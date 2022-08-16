762975bcfdf3ec5cecc95cc1426b1aef1f52dc5f
roshila <79713928+roshila@users.noreply.github.com>
2022-08-16 17:25:17 +0300

    Строку в число

--- py/hello.py
+++ py/hello.py
@@ -1,7 +1,5 @@
-result1 = 50 + 125
-result2 = str(result1)
-print("Result" + result2)
-
-result = 123 + 41
-result = str(result)
-print("Так тоже можно" + result)
+a = input()
+b = input() 
+a = int(a)
+b = int(b)
+print(a+b)
