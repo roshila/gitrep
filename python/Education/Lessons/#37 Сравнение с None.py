2e82e43261c7fe10fc585d6cb3fe69a3603a7d89
roshila <79713928+roshila@users.noreply.github.com>
2022-08-21 20:13:58 +0300

    Python day 6

--- python/Education/Lessons/#37 Сравнение с None.py
+++
@@ -1,8 +0,0 @@
-# проверить переменные на равенство или неравенство с None можно с помощью операторов is и is not
-a = None
-if a is None:
-    print('a is None')
-#
-a = 1
-if a is not None:
-    print('a is not None')
