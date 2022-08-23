4d34a0b4002edcd41057363f30ab5753e2b1e935
roshila <79713928+roshila@users.noreply.github.com>
2022-08-23 20:33:44 +0300

    Python day 7 ( с температурой 39 особо не поучишь)

---
+++ python/Education/Lessons/#14 Длина строки.py
@@ -0,0 +1,8 @@
+# Функция, определяющая длину строки называется len()
+string = input()
+string_len = len(string)
+print(string_len)
+#
+string = input()
+if not string:
+    print('Ошибка, ничего не ввёл')
