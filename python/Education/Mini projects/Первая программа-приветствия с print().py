d81ffa19f64bb822636d986279b3f8e587f19d07
roshila <79713928+roshila@users.noreply.github.com>
2022-08-30 21:57:07 +0300

    Python day 10

---
+++ python/Education/Mini projects/Первая программа-приветствия с print().py
@@ -0,0 +1,22 @@
+#
+hello = 'Привет,'
+name = input()
+greeting = hello + name 
+print(greeting)
+
+#Можно сделать короче
+name = input('what is your name?')
+greeting = 'Hello,' + name 
+print(greeting)
+
+#Можно сделать ещё короче
+name = input('what is your name?')
+print('hello,' + name)
+
+#МОЖНО ЕЩЁ КОРОЧЕ, ОДНОСТРОЧНЫЙ КОД РУЛИТ
+print('hello, ' + input('what is your name?'))
+
+hello = 'Привет пользователь'
+name = input('представься')
+greeting = ("Привет," + name)
+print(greeting)
